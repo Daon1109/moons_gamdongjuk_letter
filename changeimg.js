c1862ed0fcca3fcec51493b38f1df3f1b481a3a0
Daon1109 <86394944+Daon1109@users.noreply.github.com>
2022-01-30 21:55:26 +0900

    Add files via upload

---
+++ changeimg.js
@@ -0,0 +1,20 @@
+function cImg(self) {
+  if(document.querySelector('body').className ==='Yes_HB') {
+    document.querySelector('body').className = 'No_HB';
+    document.body.style.backgroundImage = "url('nohorseballon.png')";
+  }
+  else {
+    document.querySelector('body').className = 'Yes_HB';
+    document.body.style.backgroundImage = "url('min_final.png')";
+  }
+}
+function cImg2(self) {
+  if(document.querySelector('body').className ==='Yes_HB') {
+    document.querySelector('body').className = 'No_HB';
+    document.body.style.backgroundImage = "url('nohorseballon.png')";
+  }
+  else {
+    document.querySelector('body').className = 'Yes_HB';
+    document.body.style.backgroundImage = "url('gam_min_final.png')";
+  }
+}
